db935904326a9114df7542cfd961b146cb281494
PedroAz <pdegutty@gmail.com>
2023-02-17 15:23:27 -0300

    CSRF

--- setup/settings.py
+++ setup/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'widget_tweaks',
 ]
 
+
+CSRF_TRUSTED_ORIGINS = ['https://appviagem-production.up.railway.app/']
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
